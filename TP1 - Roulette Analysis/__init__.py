099be8f9ed95f16d0fae91d246e084845c745231
Nicolás Antonelli <niconelli2@gmail.com>
2020-03-27 23:59:10 -0300

    Added Subplots, Improved Graphs

--- TP1 - Roulette Analysis/__init__.py
+++ TP1 - Roulette Analysis/__init__.py
@@ -14,32 +14,25 @@ import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Graphication Function
-def graph(array, expected_val, title, ylabel):
-    plt.plot(array)
-    # plt.stem(array)
-    plt.axhline(expected_val, color='red', linestyle='--')
-    plt.grid(True)
-    plt.title(title)
-    plt.axis([-1, len(array), 0, max(array)*1.1])
-    plt.xlabel("Tiradas de Ruleta")
-    plt.ylabel(ylabel)
-    plt.show()
-
 if __name__ == "__main__":
-    print("Roulette Simulation (Numeral Analysis)")
+    print("Simulación de Ruleta")
 
     # Total Iterations
-    iterations = abs(int(input("Iterations (example=100000): ")))
+    iterations = abs(int(input("Iteraciones (Tiradas): ")))
 
     # All 'European' Roulette Numbers from 0 to 36
     numbers = np.arange(0, 36)
 
     # Expected Values
     rfr_expected = round(1/len(numbers), 3)     # Relative Frecuency Expected
-    avg_expected = round(np.mean(numbers), 3)   # Average (Mean) Expected
-    var_expected = round(np.var(numbers), 3)    # Deviation Expected
-    dev_expected = round(np.std(numbers), 3)    # Variance Expected
+    print("Frecuencia Relativa Esperada: "+str(rfr_expected))
+    # avg_expected = round(np.mean(numbers), 3)   # Average (Mean) Expected
+    # print("Valor Promedio Esperado: "+str(avg_expected))
+    # var_expected = round(np.var(numbers), 3)    # Deviation Expected
+    # print("Varianza Esperada: "+str(var_expected))
+    # dev_expected = round(np.std(numbers), 3)    # Variance Expected
+    # print("Desvío Esperado: "+str(dev_expected))
+    print()
 
     # I need any number for the Analysis too, so I will use a Random One
     one_number = rand.randint(0, 36)
@@ -61,15 +54,29 @@ if __name__ == "__main__":
         var_array.append(round(np.var(numbers), 6))
         dev_array.append(round(np.std(numbers), 6))
         numbers = np.zeros(37, dtype=int) # Reset for all Obtained Roulette's Values
+    
+    # Graphs
+    fig, axs = plt.subplots(2, 2)
+    fig.canvas.set_window_title("Simulación de Ruleta: Análisis (de 0 a "+str(iterations)+" tiradas)")
+    axs[0, 0].plot(rfr_array, 'tab:blue')
+    axs[0, 0].set_title("Frecuencia Relativa")
+    axs[0, 0].axhline(rfr_expected, color='red', linestyle='--')
+    plt.setp(axs[0, 0], ylabel="FR del Número "+str(one_number))
 
-    # Show the Relative Frecuency (Of One Number) Array
-    graph(rfr_array, rfr_expected, "ANÁLISIS DE FRECUENCIA (0 A "+str(iterations)+" TIRADAS)", "Frecuencia Relativa del Número "+str(one_number))
-    
-    # Show the Averages (Means) Array
-    graph(avg_array, avg_expected, "ANÁLISIS DE LA MEDIA (0 A "+str(iterations)+" TIRADAS)", "Valor Promedio de las Tiradas")
-    
-    # Show the Variance Array
-    graph(var_array, var_expected, "ANÁLISIS DE LA VARIANZA (0 A "+str(iterations)+" TIRADAS)", "Varianza de las Tiradas")
-    
-    # Show the Deviation Array
-    graph(dev_array, dev_expected, "ANÁLISIS DE LA DESVIACIÓN (0 A "+str(iterations)+" TIRADAS)", "Desvío de las Tiradas")
+    axs[0, 1].plot(avg_array, 'tab:orange')
+    axs[0, 1].set_title('Valor Promedio')
+    # axs[0, 1].axhline(avg_expected, color='red', linestyle='--')
+
+    axs[1, 0].plot(var_array, 'tab:green')
+    axs[1, 0].set_title('Varianza')
+    # axs[1, 0].axhline(var_expected, color='red', linestyle='--')
+
+    axs[1, 1].plot(dev_array, 'tab:purple')
+    axs[1, 1].set_title('Desvío')
+    # axs[1, 1].axhline(dev_expected, color='red', linestyle='--')
+
+    for ax in fig.get_axes():
+        ax.grid(True)
+        plt.setp(ax, xlabel="Número de Tiradas")
+    fig.tight_layout()
+    plt.show()
