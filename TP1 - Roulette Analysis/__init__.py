8cc92fecc7ea9fcee024b7df46c3f5caa37a438d
Nicolás Antonelli <niconelli2@gmail.com>
2020-04-05 05:29:11 -0300

    Optimized Code and Complexity, Deleted Manual Functions, Added Time

--- TP1 - Roulette Analysis/__init__.py
+++ TP1 - Roulette Analysis/__init__.py
@@ -12,45 +12,14 @@ Final Date: 15/04/2020
 Python Libraries/Modules Used:
     - Numpy: Random Numbers, Statistical Functions, Array Manipulation
     - Pyplot: Matplotlib Module for Graph Plotting
+    - Time: Module used for processing time measurement
 
 Note: All Printed Messages and Plotted Graphs are in SPANISH
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# Functions for Statisical Calcutations WITH FECUENCY
-
-def average_with_frecuency(array, factor):
-    total = 0
-    # Mathematic Spectation (Discrete Variable) Ecuation
-    for i in range(len(array)):
-        total += i*array[i]
-    total /= factor
-    return round(total, 3)
-
-def variance_with_frecuency(array, factor):
-    total = 0
-    mean = average_with_frecuency(array, factor)
-    # Variance (Discrete Variable) Ecuation
-    for i in range(len(array)):
-        total += ((i - mean)**2) * array[i]
-    total /= factor
-    return round(total, 3)
-
-def deviation_with_frecuency(variance):
-    # Deviation is the Square Root of Variance
-    return (variance**0.5)
-
-# def variance_with_frecuency_alternative(array, factor):
-#     total = 0
-#     mean = average_with_frecuency(array, factor)
-#     for i in range(len(array)):
-#         total += i*i*array[i]
-#     total /= factor
-#     total = total-mean**2
-#     return round(total, 3)
+import time
 
 
 if __name__ == "__main__":
@@ -61,10 +30,10 @@ if __name__ == "__main__":
     numbers = np.arange(0, 37)
     
     # Expected Values
-    rfr_expected = round(1/len(numbers), 3)     # Relative Frecuency Expected (1/37)
-    avg_expected = round(np.mean(numbers), 3)   # Average (Mean) Expected (666/37)
-    var_expected = round(np.var(numbers), 3)    # Variance Expected (114)
-    dev_expected = round(np.std(numbers), 3)    # Deviation Expected (Square Root of 114)
+    rfr_expected = round(1/len(numbers), 6)     # Relative Frecuency Expected (1/37)
+    avg_expected = round(np.mean(numbers), 6)   # Average (Mean) Expected (666/37)
+    var_expected = round(np.var(numbers), 6)    # Variance Expected (114)
+    dev_expected = round(np.std(numbers), 6)    # Deviation Expected (Square Root of 114)
     print("Frecuencia Relativa Esperada: "+str(rfr_expected))
     print("Valor Promedio Esperado: "+str(avg_expected))
     print("Varianza Esperada: "+str(var_expected))
@@ -75,7 +44,7 @@ if __name__ == "__main__":
     # Recommended: Between 5 and 10
     results = 6
 
-    # Total Iterations
+    # Total Iterations (Example: Try with 500)
     iterations = 0
     while (iterations==0):
         # Validation
@@ -88,29 +57,38 @@ if __name__ == "__main__":
         # Validation
         one_number = abs(int(float(input("Ingrese su Número (Entre 0 y 36): "))))
     print()
+    
+    # Timer (Execution Time Measurement)
+    start_time = time.time()
 
     # For Every Iteration: Mean-Deviaton-Variance Values & Relative Frecuency of One Number
     # Matix Re-Size: Arrays of Arrays, Needed for the Graph with few "Results" simultaniously
-    rfr_array = np.zeros((6, iterations+1)) # Relative Frecuencies of One Number Array Initialization
-    avg_array = np.zeros((6, iterations+1)) # Average (Mean) Arry Initialization
-    var_array = np.zeros((6, iterations+1)) # Variance Array Initialization
-    dev_array = np.zeros((6, iterations+1)) # Deviation Array Initialization
+    rfr_array = np.zeros((results, iterations+1)) # Relative Frecuencies of One Number Array Initialization
+    avg_array = np.zeros((results, iterations+1)) # Average (Mean) Arry Initialization
+    var_array = np.zeros((results, iterations+1)) # Variance Array Initialization
+    dev_array = np.zeros((results, iterations+1)) # Deviation Array Initialization
     roulette_values = np.zeros(37, dtype=int) # Initialization for Obtained Roulette's Values
     print("Cargando "+str(iterations)+" iteraciones...")
+    acum_load_time = 0 # Variable Needed for Time Measurement
     for i in range(results):
         for j in range(1, iterations+1):
-            for _ in range(0, j):
-                # Playing Roulette and Analyzing the Obtained Numbers
-                n = np.random.randint(0, 37)
-                roulette_values[n] += 1
+            # Playing Roulette and Analyzing the Obtained Numbers
+            roulette_values = np.random.randint(0, 37, j) # Random Numbers Generator (Total = J Numbers)
+            # Occurrences Counter of One_Number (The Input Number)
+            one_number_occurrences = np.count_nonzero(roulette_values == one_number)
             # Filling Important Arrays (In the Matrix Position for the Actual Results Array)
-            rfr_array[i][j] = roulette_values[one_number]/sum(roulette_values)
-            avg_array[i][j] = round(average_with_frecuency(roulette_values, j), 6)
-            variance = round(variance_with_frecuency(roulette_values, j), 6)
-            var_array[i][j] = variance
-            dev_array[i][j] = round(deviation_with_frecuency(variance), 6)
-            roulette_values = np.zeros(37, dtype=int) # Reset for all Obtained Roulette's Values
-        print("Result "+str(i+1)+" of "+str(results)+" loaded")
+            rfr_array[i][j] = one_number_occurrences / j
+            avg_array[i][j] = round(np.mean(roulette_values), 6)
+            var_array[i][j] = variance = round(np.var(roulette_values), 6)
+            dev_array[i][j] = round(variance ** 0.5, 6) # Standard Deviation is the Square Root of Variance
+        # Calculate and Show intermediate processing Time
+        last_load_time = time.time() - start_time - acum_load_time
+        acum_load_time = time.time() - start_time
+        print("Array Resultado "+str(i+1)+" de "+str(results)+" completado en "+str(round(last_load_time, 6))+ " segundos")
+
+    # Total Calculation Time
+    final_time = round(time.time() - start_time, 6)
+    print("Todos los Arrays de Resultados fueron completados en "+str(final_time)+ " segundos")
     print()
     
     # First Array of Results' Graphs
