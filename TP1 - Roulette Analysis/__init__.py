87356d137dd01965b3bb1a8f9bfce99c8520c6b2
Nicolás Antonelli <niconelli2@gmail.com>
2020-04-06 20:02:18 -0300

    Code Modularization, Global Variables, Save: Try-Except, Minor Changes

--- TP1 - Roulette Analysis/__init__.py
+++ TP1 - Roulette Analysis/__init__.py
@@ -22,6 +22,139 @@ import matplotlib.pyplot as plt
 import time
 
 
+# Global Variables
+calculate_time = True   # Time Measurement (True = Yes, False = No)
+save = True             # Save Graphics (True = Yes, False = No)
+save_route = "graphs/"  # Graphs Folder
+
+
+# Mean (Average)
+def calc_mean(population):
+    return np.mean(population)
+
+# Variance
+def calc_variance(population):
+    return np.var(population)
+
+# Deviation
+def calc_deviation(population):
+    return np.std(population)
+
+# Deviation from Variance
+def calc_deviation_from_variance(variance):
+    # Standard Deviation is the square root of the Variance
+    return (variance ** 0.5)
+
+# Load (for Plotting) One Array of Results
+# Graphs Loaded: Relative Frecuency, Mean, Variance, Deviation (4-Graph Stack)
+def load_one_result_graphs(frecuencies, averages, variances, deviations, save):
+    # Subplots and Title Configs
+    fig, axs = plt.subplots(2, 2)
+    fig.canvas.set_window_title("Simulación de Ruleta: Análisis (de 0 a "+str(iterations)+" tiradas) - Stack 1")
+
+    # Relative Frecuencies Graph
+    axs[0, 0].plot(frecuencies, 'tab:blue', label="FRN")
+    axs[0, 0].set_title("Frecuencia Relativa")
+    axs[0, 0].axhline(rfr_expected, color='red', linestyle='--', label="FRE")
+    plt.setp(axs[0, 0], ylabel="FR del Número "+str(one_number))
+
+    # Averages Graph
+    axs[0, 1].plot(averages, 'tab:orange', label="VPN")
+    axs[0, 1].set_title('Valor Promedio')
+    axs[0, 1].axhline(avg_expected, color='red', linestyle='--', label="VPE")
+
+    # Variances Graph
+    axs[1, 0].plot(variances, 'tab:green', label="VVN")
+    axs[1, 0].set_title('Varianza')
+    axs[1, 0].axhline(var_expected, color='red', linestyle='--', label="VVE")
+
+    # Deviations Graph
+    axs[1, 1].plot(deviations, 'tab:purple', label="VDN")
+    axs[1, 1].set_title('Desvío')
+    axs[1, 1].axhline(dev_expected, color='red', linestyle='--', label="VDE")
+    
+    # Details Config
+    for ax in fig.get_axes():
+        ax.grid(True)
+        ax.legend()
+        plt.setp(ax, xlabel="Número de Tiradas")
+    fig.tight_layout()
+    
+    # Save Plot (PNG Image)
+    if save:
+        try:
+            plt.savefig(save_route+"graph_iterations_"+str(iterations)+".png")
+            print("1er 4-Stack de Gráficos Guardado Correctamente")
+        except:
+            print("1er 4-Stack de Gráficos NO fue guardado porque hubo un problema")
+        print()
+
+# Load (for Plotting) Every Array of Results Simultaneously
+# Graphs Loaded: Relative Frecuency, Mean, Variance, Deviation (4-Graph Stack)
+def load_every_result_graphs(frecuencies, averages, variances, deviations, save):
+    # Subplots and Title Configs
+    fig, axs = plt.subplots(2, 2)
+    fig.canvas.set_window_title("Simulación de Ruleta: Análisis (de 0 a "+str(iterations)+" tiradas) - Stack 2")
+
+    # Relative Frecuencies Graph
+    axs[0, 0].plot(frecuencies[0], 'tab:blue')
+    axs[0, 0].plot(frecuencies[1], 'tab:orange')
+    axs[0, 0].plot(frecuencies[2], 'tab:green')
+    axs[0, 0].plot(frecuencies[3], 'tab:purple')
+    axs[0, 0].plot(frecuencies[4], 'tab:pink')
+    axs[0, 0].plot(frecuencies[5], 'tab:brown')
+    axs[0, 0].set_title("Frecuencia Relativa")
+    axs[0, 0].axhline(rfr_expected, color='red', linestyle='--', label="FRE")
+    plt.setp(axs[0, 0], ylabel="FR del Número "+str(one_number))
+
+    # Averages Graph
+    axs[0, 1].plot(averages[0], 'tab:blue')
+    axs[0, 1].plot(averages[1], 'tab:orange')
+    axs[0, 1].plot(averages[2], 'tab:green')
+    axs[0, 1].plot(averages[3], 'tab:purple')
+    axs[0, 1].plot(averages[4], 'tab:pink')
+    axs[0, 1].plot(averages[5], 'tab:brown')
+    axs[0, 1].set_title('Valor Promedio')
+    axs[0, 1].axhline(avg_expected, color='red', linestyle='--', label="VPE")
+
+    # Variances Graph
+    axs[1, 0].plot(variances[0], 'tab:blue')
+    axs[1, 0].plot(variances[1], 'tab:orange')
+    axs[1, 0].plot(variances[2], 'tab:green')
+    axs[1, 0].plot(variances[3], 'tab:purple')
+    axs[1, 0].plot(variances[4], 'tab:pink')
+    axs[1, 0].plot(variances[5], 'tab:brown')
+    axs[1, 0].set_title('Varianza')
+    axs[1, 0].axhline(var_expected, color='red', linestyle='--', label="VVE")
+
+    # Deviations Graph
+    axs[1, 1].plot(deviations[0], 'tab:blue')
+    axs[1, 1].plot(deviations[1], 'tab:orange')
+    axs[1, 1].plot(deviations[2], 'tab:green')
+    axs[1, 1].plot(deviations[3], 'tab:purple')
+    axs[1, 1].plot(deviations[4], 'tab:pink')
+    axs[1, 1].plot(deviations[5], 'tab:brown')
+    axs[1, 1].set_title('Desvío')
+    axs[1, 1].axhline(dev_expected, color='red', linestyle='--', label="VDE")
+    
+    # Details Config
+    for ax in fig.get_axes():
+        ax.grid(True)
+        ax.legend()
+        plt.setp(ax, xlabel="Número de Tiradas")
+    fig.tight_layout()
+
+    # Save Plot (PNG Image)
+    if save:
+        try:
+            plt.savefig(save_route+"multi_graph_iterations_"+str(iterations)+".png")
+            print("2do 4-Stack de Gráficos Guardado Correctamente")
+        except:
+            print("2do 4-Stack de Gráficos NO fue guardado porque hubo un problema")
+        print()
+
+
+# Main
 if __name__ == "__main__":
     print("SIMULACIÓN DE RULETA")
     print()
@@ -30,10 +163,10 @@ if __name__ == "__main__":
     numbers = np.arange(0, 37)
     
     # Expected Values
-    rfr_expected = round(1/len(numbers), 6)     # Relative Frecuency Expected (1/37)
-    avg_expected = round(np.mean(numbers), 6)   # Average (Mean) Expected (666/37)
-    var_expected = round(np.var(numbers), 6)    # Variance Expected (114)
-    dev_expected = round(np.std(numbers), 6)    # Deviation Expected (Square Root of 114)
+    rfr_expected = round(1/len(numbers), 6)             # Relative Frecuency Expected (1/37)
+    avg_expected = round(calc_mean(numbers), 6)         # Average (Mean) Expected (666/37)
+    var_expected = round(calc_variance(numbers), 6)     # Variance Expected (114)
+    dev_expected = round(calc_deviation(numbers), 6)    # Deviation Expected (Square Root of 114)
     print("Frecuencia Relativa Esperada: "+str(rfr_expected))
     print("Valor Promedio Esperado: "+str(avg_expected))
     print("Varianza Esperada: "+str(var_expected))
@@ -77,101 +210,32 @@ if __name__ == "__main__":
             # Occurrences Counter of One_Number (The Input Number)
             one_number_occurrences = np.count_nonzero(roulette_values == one_number)
             # Filling Important Arrays (In the Matrix Position for the Actual Results Array)
-            rfr_array[i][n] = one_number_occurrences / n                    # Relative Frecuency of "One_Number"
-            avg_array[i][n] = round(np.mean(roulette_values), 6)            # Average (Mean)
-            var_array[i][n] = variance = round(np.var(roulette_values), 6)  # Variance
-            dev_array[i][n] = round(variance ** 0.5, 6)                     # Standard Deviation (Square Root of Variance)
-        # Calculate and Show intermediate processing Time
-        last_load_time = time.time() - start_time - acum_load_time
-        acum_load_time = time.time() - start_time
-        print("Array Resultado "+str(i+1)+" de "+str(results)+" completado en "+str(round(last_load_time, 6))+ " segundos")
+            rfr_array[i][n] = one_number_occurrences / n                            # Relative Frecuency of "One_Number"
+            avg_array[i][n] = round(calc_mean(roulette_values), 6)                  # Average (Mean)
+            var_array[i][n] = variance = round(calc_variance(roulette_values), 6)   # Variance
+            dev_array[i][n] = round(calc_deviation_from_variance(variance), 6)      # Standard Deviation (Square Root of Variance)
+            
+        # Feedback: Calculate and Show intermediate processing Time
+        if calculate_time:
+            last_load_time = time.time() - start_time - acum_load_time
+            acum_load_time = time.time() - start_time
+            print("Array Resultado "+str(i+1)+" de "+str(results)+" completado en "+str(round(last_load_time, 6))+ " segundos")
+        else:
+            print("Array Resultado "+str(i+1)+" de "+str(results)+" completado")
 
     # Total Calculation Time
-    final_time = round(time.time() - start_time, 6)
-    print("Todos los Arrays de Resultados fueron completados en "+str(final_time)+ " segundos")
+    if calculate_time:
+        final_time = round(time.time() - start_time, 6)
+        print("Todos los Arrays de Resultados fueron completados en "+str(final_time)+ " segundos")
+    else:
+        print("Todos los Arrays de Resultados fueron completados")
     print()
     
     # First Array of Results' Graphs
-    fig, axs = plt.subplots(2, 2)
-    fig.canvas.set_window_title("Simulación de Ruleta: Análisis (de 0 a "+str(iterations)+" tiradas) - Stack 1")
-
-    axs[0, 0].plot(rfr_array[0], 'tab:blue', label="FRN")
-    axs[0, 0].set_title("Frecuencia Relativa")
-    axs[0, 0].axhline(rfr_expected, color='red', linestyle='--', label="FRE")
-    plt.setp(axs[0, 0], ylabel="FR del Número "+str(one_number))
-
-    axs[0, 1].plot(avg_array[0], 'tab:orange', label="VPN")
-    axs[0, 1].set_title('Valor Promedio')
-    axs[0, 1].axhline(avg_expected, color='red', linestyle='--', label="VPE")
-
-    axs[1, 0].plot(var_array[0], 'tab:green', label="VVN")
-    axs[1, 0].set_title('Varianza')
-    axs[1, 0].axhline(var_expected, color='red', linestyle='--', label="VVE")
-
-    axs[1, 1].plot(dev_array[0], 'tab:purple', label="VDN")
-    axs[1, 1].set_title('Desvío')
-    axs[1, 1].axhline(dev_expected, color='red', linestyle='--', label="VDE")
+    load_one_result_graphs(rfr_array[0], avg_array[0], var_array[0], dev_array[0], save)
     
-    for ax in fig.get_axes():
-        ax.grid(True)
-        ax.legend()
-        plt.setp(ax, xlabel="Número de Tiradas")
-    
-    fig.tight_layout()
-    plt.savefig("TP1 - Roulette Analysis/graphs/graph_iterations_"+str(iterations)+".png")
-    print("1er 4-Stack de Gráficos Guardado Correctamente")
-    print()
-
     # Now, Comparing all the Results Array at the same Time
-    fig, axs = plt.subplots(2, 2)
-    fig.canvas.set_window_title("Simulación de Ruleta: Análisis (de 0 a "+str(iterations)+" tiradas) - Stack 2")
-
-    axs[0, 0].plot(rfr_array[0], 'tab:blue')
-    axs[0, 0].plot(rfr_array[1], 'tab:orange')
-    axs[0, 0].plot(rfr_array[2], 'tab:green')
-    axs[0, 0].plot(rfr_array[3], 'tab:purple')
-    axs[0, 0].plot(rfr_array[4], 'tab:pink')
-    axs[0, 0].plot(rfr_array[5], 'tab:brown')
-    axs[0, 0].set_title("Frecuencia Relativa")
-    axs[0, 0].axhline(rfr_expected, color='red', linestyle='--', label="FRE")
-    plt.setp(axs[0, 0], ylabel="FR del Número "+str(one_number))
-
-    axs[0, 1].plot(avg_array[0], 'tab:blue')
-    axs[0, 1].plot(avg_array[1], 'tab:orange')
-    axs[0, 1].plot(avg_array[2], 'tab:green')
-    axs[0, 1].plot(avg_array[3], 'tab:purple')
-    axs[0, 1].plot(avg_array[4], 'tab:pink')
-    axs[0, 1].plot(avg_array[5], 'tab:brown')
-    axs[0, 1].set_title('Valor Promedio')
-    axs[0, 1].axhline(avg_expected, color='red', linestyle='--', label="VPE")
-
-    axs[1, 0].plot(var_array[0], 'tab:blue')
-    axs[1, 0].plot(var_array[1], 'tab:orange')
-    axs[1, 0].plot(var_array[2], 'tab:green')
-    axs[1, 0].plot(var_array[3], 'tab:purple')
-    axs[1, 0].plot(var_array[4], 'tab:pink')
-    axs[1, 0].plot(var_array[5], 'tab:brown')
-    axs[1, 0].set_title('Varianza')
-    axs[1, 0].axhline(var_expected, color='red', linestyle='--', label="VVE")
-
-    axs[1, 1].plot(dev_array[0], 'tab:blue')
-    axs[1, 1].plot(dev_array[1], 'tab:orange')
-    axs[1, 1].plot(dev_array[2], 'tab:green')
-    axs[1, 1].plot(dev_array[3], 'tab:purple')
-    axs[1, 1].plot(dev_array[4], 'tab:pink')
-    axs[1, 1].plot(dev_array[5], 'tab:brown')
-    axs[1, 1].set_title('Desvío')
-    axs[1, 1].axhline(dev_expected, color='red', linestyle='--', label="VDE")
-    
-    for ax in fig.get_axes():
-        ax.grid(True)
-        ax.legend()
-        plt.setp(ax, xlabel="Número de Tiradas")
-    
-    fig.tight_layout()
-    plt.savefig("TP1 - Roulette Analysis/graphs/multi_graph_iterations_"+str(iterations)+".png")
-    print("2do 4-Stack de Gráficos Guardado Correctamente")
-    print()
+    load_every_result_graphs(rfr_array, avg_array, var_array, dev_array, save)
 
     # Plotting all the Graphs
     plt.show()
